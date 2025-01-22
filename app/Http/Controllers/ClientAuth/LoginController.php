c7134c65f7ecfc2118fb7211f5308dd533394d93
karimaDiab <karima20110267@gmail.com>
2025-01-22 12:25:43 +0200

    lllllll

---
+++ app/Http/Controllers/ClientAuth/LoginController.php
@@ -0,0 +1,36 @@
+<?php
+namespace App\Http\Controllers\ClientAuth;
+
+use App\Models\Client;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+use App\Http\Controllers\Controller;
+
+
+class LoginController extends Controller
+{
+    public function showLoginForm()
+    {
+       
+        return view('ClientDashboard.login');
+    }
+
+    public function login(Request $request)
+    {  
+        $credentials = $request->only('email','password');
+      
+        if (Auth::guard('client')->attempt($credentials)) {
+            //  dd($request->all());
+            return redirect()->route('client.index');
+        }
+        // dd($credentials);
+        return redirect()->route('client.login')->withErrors(['error' => 'Invalid credentials']);
+    }
+
+    public function logout()
+    {
+        Auth::guard('client')->logout();
+        return redirect()->route('client.login');
+    }
+}
--- routes/client.php
+++ routes/client.php
@@ -1,7 +1,7 @@
 <?php
 // require __DIR__ . '/auth.php';
 
-use App\Http\Controllers\ClientAuth\ClientAuthController;
+use App\Http\Controllers\ClientAuth\LoginController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -16,15 +16,13 @@ use App\Http\Controllers\ClientAuth\ClientAuthController;
 //     return view('middleware.login');
 // });
 
-    Route::get('clientDash/login', [ClientAuthController::class, 'showLoginForm']);
-    Route::post('clientDash/login', [ClientAuthController::class, 'login'])->name('client.login');
-    // Route::get('clientDash/index', [ClientAuthController::class, 'index'])->name('client.index');
-    Route::post('clientDash/logout', [ClientAuthController::class, 'logout'])->name('client.logout');
-
+    Route::get('clientDash/login', [LoginController::class, 'showLoginForm']);
+    Route::post('clientDash/login', [LoginController::class, 'login'])->name('client.login');
+    // Route::get('clientDash/index', [LoginController::class, 'index'])->name('client.index');
     Route::get('/clientDash/index', function () {
         return view('clientDashboard.index');
     })->name('client.index');
-    
+    Route::post('clientDash/logout', [LoginController::class, 'logout'])->name('client.logout');
 // Route::prefix('clientDash')->name('client.')->group(function () {
 //     // dd("client");
    
